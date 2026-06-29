8b59559a3de226115df2d2a1d2800b699f58c3d3
moeez-shafiquee <moeezshafique.pk@gmail.com>
2026-06-29 15:33:45 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict" // This mean use all JS code as a newer version
+
+// alert ( 3 + 3) we are using nodejs not browser
+
+// console.log("Moeez"); console.log("3+3");  Don't do this code readability should be high always start with next line
+
+// console.log ("3 + 
+// 
+// 3")  Also don't do this its look bad to break code into different lines
+
+let name = "Moeez"  // string
+let age = 23  // number
+let isLoggedIn = true  // boolean
+let city;  // undifined
+let weather = null  // null
+
+// Premitive Datatypes
+
+// string => ""  Use as single or double coat but preferable in double coats
+// number => 2 to power 53 
+// bigint => use for bigger numbers in trading, reddit like projects
+// boolean => true/false
+// null => standalone value
+// undifined => unassigned value
+// symbol => use for uniqueness
+
+// object =>
+
+
+console.log (typeof "")  // typeof is use to check what datatype is.
+console.log (typeof null)  // null is a object
