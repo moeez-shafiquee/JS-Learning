9d8dd6e26b11027024399773388e760039d734ae
moeez-shafiquee <moeezshafique.pk@gmail.com>
2026-06-30 09:09:21 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,57 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+// ************************** Operations *************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+
+// Basic Operations
+
+// console.log(2+2); Plus
+// console.log(2-2); Minus
+// console.log(2*2); Multiply
+// console.log(2**3); 2 to Power 3
+// console.log(2/3);  Divide
+// console.log(2%3);  Remainder
+
+
+// We can add strings but not substract
+
+let word1 = "Hello"
+let word2 = " Moeez"
+let word3 = " Shafique"
+
+let sentence = word1 + word2 + word3
+
+// console.log(sentence);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);  // if the first number in string the value is in string
+// console.log(1 + 2 + "2");  
+
+// console.log( (3 + 4) * 5 % 3); Don't write complecated operation code like this its not appreciatable.
+
+
+// console.log(+true); The value come 1 
+// console.log(+"");  The value come 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2  // Also not a good method Code should be clean and future proof
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
